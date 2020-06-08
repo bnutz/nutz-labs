a2613ec3194565f750b13f341633095695404be0
Brian Lau <bnutz@users.noreply.github.com>
2020-06-08 17:08:51 +0800

    Refactor WikiModel

--- app/src/main/java/com/justbnutz/labs/models/WikipediaModel.kt
+++ app/src/main/java/com/justbnutz/labs/models/WikipediaModel.kt
@@ -4,38 +4,55 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class WikiModel (
-    val batchcomplete : String?,
+data class WikiModel(
+    @Json(name = "batchcomplete")
+    val batchcomplete: String?,
     @Json(name = "continue")
-    val cont : Continue?,
-    val query : Query?
-)
+    val cont: Continue?,
+    @Json(name = "query")
+    val query: Query?
+) {
+    @JsonClass(generateAdapter = true)
+    data class Continue(
+        @Json(name = "sroffset")
+        val sroffset: Int?,
+        @Json(name = "continue")
+        val cont: String?
+    )
 
-@JsonClass(generateAdapter = true)
-data class Continue (
-    val sroffset : Int?,
-    @Json(name = "continue")
-    val cont : String?
-)
+    @JsonClass(generateAdapter = true)
+    data class Query(
+        @Json(name = "searchinfo")
+        val searchinfo: Searchinfo?,
+        @Json(name = "search")
+        val search: List<Search>?
+    ) {
+        @JsonClass(generateAdapter = true)
+        data class Searchinfo(
+            @Json(name = "totalhits")
+            val totalhits: Int?,
+            @Json(name = "suggestion")
+            val suggestion: String?,
+            @Json(name = "suggestionsnippet")
+            val suggestionsnippet: String?
+        )
 
-@JsonClass(generateAdapter = true)
-data class Query (
-    val searchinfo : Searchinfo?,
-    val search : List<Search>?
-)
-
-@JsonClass(generateAdapter = true)
-data class Searchinfo (
-    val totalhits : Int?
-)
-
-@JsonClass(generateAdapter = true)
-data class Search (
-    val ns : Int?,
-    val title : String?,
-    val pageid : Int?,
-    val size : Int?,
-    val wordcount : Int?,
-    val snippet : String?,
-    val timestamp : String?
-)
+        @JsonClass(generateAdapter = true)
+        data class Search(
+            @Json(name = "ns")
+            val ns: Int?,
+            @Json(name = "title")
+            val title: String?,
+            @Json(name = "pageid")
+            val pageid: Int?,
+            @Json(name = "size")
+            val size: Int?,
+            @Json(name = "wordcount")
+            val wordcount: Int?,
+            @Json(name = "snippet")
+            val snippet: String?,
+            @Json(name = "timestamp")
+            val timestamp: String?
+        )
+    }
+}
